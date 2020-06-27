2634857af9aedef649ca36afe396bbaab70c4d1d
Manomay2323 <66709511+Manomay2323@users.noreply.github.com>
2020-06-27 11:01:32 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,42 @@
+class Ground {
+constructor(x,y,width,height){   
+var options={
+isStatic:true
+}
+
+
+
+this.body=Bodies.rectangle(x,y,width,height,options);
+World.add(world,this.body);
+}
+
+
+display(){
+    fill(255);
+    rectMode(CENTER);  
+    rect(this.body.position.x,this.body.position.y,this.width,this.height);
+}
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
